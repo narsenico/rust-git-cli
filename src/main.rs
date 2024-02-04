009d7b837175c1692020fa762b7a63381c49508f
narsenico <Gianfranco.Caldi@zucchetti.it>
2024-02-04 23:41:52 +0100

    First commit

---
+++ src/main.rs
@@ -0,0 +1,48 @@
+use std::{env, error::Error};
+
+use git2::{Branch, BranchType, Repository};
+
+type Result<T> = std::result::Result<T, Box<dyn Error>>;
+
+#[derive(Debug, Default)]
+struct BranchInfo {
+    name: String,
+    local: bool,
+    current: bool,
+}
+
+impl BranchInfo {
+    fn from(branch: (Branch, BranchType)) -> Option<Self> {
+        if let Ok(Some(name)) = branch.0.name() {
+            return Some(Self {
+                name: name.to_string(),
+                local: branch.1 == BranchType::Local,
+                current: branch.0.is_head(),
+            });
+        }
+
+        None
+    }
+}
+
+fn get_local_branches(repo: &Repository) -> Result<Vec<BranchInfo>> {
+    let branches = repo
+        .branches(Some(BranchType::Local))?
+        .filter_map(std::result::Result::ok)
+        .map(BranchInfo::from)
+        .flatten()
+        .collect::<Vec<BranchInfo>>();
+
+    Ok(branches)
+}
+
+fn main() -> Result<()> {
+    let mut args = env::args();
+    let repo_path = args.nth(1).unwrap();
+    let repo = Repository::open(&repo_path)?;
+    let branches = get_local_branches(&repo)?;
+    let current_branch = branches.iter().find(|b| b.current);
+    println!("local branches: {:?}", branches);
+    println!("current branch: {:?}", current_branch);
+    Ok(())
+}

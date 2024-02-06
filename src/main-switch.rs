8a8ea49cdbf3cecf56b2ecaf1dd0acc6ea101e75
narsenico <Gianfranco.Caldi@zucchetti.it>
2024-02-06 23:46:19 +0100

    Impl switch

--- src/main-switch.rs
+++ src/main-switch.rs
@@ -1,14 +1,47 @@
 mod common;
 
-use std::env;
-use git2::{Branch, BranchType, Repository};
-use common::Result;
+use std::process::ExitCode;
 
-fn _switch_branch(repo: &Repository, branch_name: &str) -> Result<()> {
-    // repo.set_head(&("refs/heads/".to_owned() + branch_name));
-    todo!("'git switch' try repo set_head")
+use common::Result;
+use dialoguer::Select;
+use git2::Repository;
+
+use crate::common::get_local_branches;
+
+fn switch_branch(repo: &Repository, branch_name: &str) -> Result<()> {
+    repo.set_head(&("refs/heads/".to_owned() + branch_name))?;
+    Ok(())
 }
 
-fn main() -> Result<()> {
-    todo!()
+fn main() -> Result<ExitCode> {
+    let repo_path = ".";
+    let repo = Repository::open(&repo_path)?;
+    let branches = get_local_branches(&repo)?;
+    let names = branches
+        .iter()
+        .filter(|b| !b.current)
+        .map(|b| b.name.as_str())
+        .collect::<Vec<&str>>();
+    let names = names.as_slice();
+
+    if names.len() == 0 {
+        eprintln!("There is no branch to switch to");
+        return Ok(ExitCode::FAILURE);
+    }
+
+    let choice = Select::new()
+        .with_prompt("Select branch to switch into")
+        .items(&names)
+        .interact_opt()?;
+
+    let Some(choice) = choice else {
+        println!("Switch cancelled");
+        return Ok(ExitCode::SUCCESS);
+    };
+
+    if let Some(selected_branch) = branches.get(choice) {
+        switch_branch(&repo, selected_branch.name.as_str())?;
+    }
+
+    Ok(ExitCode::SUCCESS)
 }
